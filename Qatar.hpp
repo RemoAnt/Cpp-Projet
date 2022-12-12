87aff59e33bdba54dec5942a287e55a0afc5439b
RemoAnt <remi.antonucci@gmail.com>
2022-12-12 17:52:53 +0100

    Update Qatar.hpp

--- Qatar.hpp
+++ Qatar.hpp
@@ -4,57 +4,73 @@
 
 class Date {
     public:
-        int get_date(); //réfléchir comment
-        int get_day(){return day;}
-        int get_month(){return month;}
-        int get_year(){return year;}
-        int set_date();
+        int getDate(); //réfléchir comment
+        int getDay(){return day;}
+        int getMonth(){return month;}
+        int getYear(){return year;}
+        int setDate();
         
     private : 
         int day;
         int month; 
         int year;
-}
+};
 
 // -------------------  Bâtiments 
 
 //Classes abstraites
 class Building { 
-    private:
-        static int id_building;
-        int time_to_built;
-        int n_workers_needed;
-}
-
-class Building : public Housing {
-    private: 
-        int n_workers;
-        
-}
-
-class Building : public Production {
     public:
-        int prod_ressource();
-}
+        int buildTimeLeft();
+        int generateCo2();
+        int build();
+        int destroy();
 
-class Building : public Rewards {
+    private:
+        static int idBuilding;
+        int scoreBuilding; 
+        int timeToBuilt;
+        int nWorkersNeeded;
+};
 
-}
+class Building : public HousingBuilding {
+    public:
+        int getnWorker(){return nWorker;}
+        int sick();
+
+    private: 
+        int nWorker;     
+};
+
+class Building : public ProductionBuilding {
+    public:
+        int generateRessource();
+};
+
+class Building : public RewardBuilding {
+    public:
+        int generateScore();
+};
 
 //Bâtiments :
 
-class Building : public Mosque {
+class HousingBuilding : public Slum{ //Bcp de chance d'être malade, et augmente nombre morts. Peu cher, grande capacité.
+    public:
+        Slum();
+        Slum(int x, int y);
+};
 
-}
+class HousingBuilding : public Appartments{ //Un peu de chance d'être malade, grande capacité, moyennement cher comparé à capacité.
+    public:
+        Appartments();
+        Appartments(int x, int y);
+};
 
-class Building : public Church {
-
-
-}
-
-class Building : public Synagogue {
-
-}
+class HousingBuilding : public House{ //Peu de chance d'être malade, cher comparé à petite capacité.
+    public:
+        House();
+        House(int x, int y);
+};
 
 // -------------------  Classes du jeu comprenant les données principales 
 
@@ -62,41 +78,41 @@ class Game {
     public:
 
         // Fonctions appelées en "temps réel" :
-        int calcul_game_score();
-        int calcul_popularity();
-        int calcul_money();
-        int calcul_petrol();
-        int calcul_time_left();
-        int calcul_n_death();
+        int calculGameScore();
+        int calculPopularity();
+        int calculMoney();
+        int calculPetrol();
+        int calculTimeLeft();
+        int calculDeath();
         int refresh();
 
         // Fonction appelée qu'à la fin
         int fin();        
 
         //Getters / Setters 
-        int get_money(){return money;}
-        int get_petrol(){return petrol;}
-        float get_popularity(){return popularity;}
-        int get_game_score(){return game_score;}
-        int get_n_stadium(){return n_stadium;}
-        int hidden_n_death(){return hidden_n_death;}
-        int get_time_left(){return time_left;}
-        Date get_deadline(){return deadline;}
-        Date get_current_date(){return current date;}
+        int getMoney(){return money;}
+        int getPetrol(){return petrol;}
+        float getPopularity(){return popularity;}
+        int getGameScore(){return gameScore;}
+        int getnStadium(){return nStadium;}
+        int getHiddenDeath(){return hiddenDeath;}
+        int getTimeLeft(){return timeLeft;}
+        Date getDeadline(){return deadline;}
+        Date getCurrentDate(){return currentDate;}
 
 
     private :
         int money;
         int petrol; 
         float popularity; //En pourcentage, dépend des bât et en débloquent certains (par exemple au bout de tant d'hôtel, on peut faire une statue)
-        int game_score;
-        int n_stadium;
-        int hidden_n_death;
-        int time_left;
+        int gameScore;
+        int nStadium;
+        int hiddenDeath;
+        int timeLeft;
         Date deadline;
-        Date current_date;
-        Batiment* l_bat[3]; //3 types de bâtiments différents
-}
+        Date currentDate;
+        Building* l_bat[3]; //3 types de bâtiments différents
+};
 
 
 //Il faut trouver comment faire une surcharge
